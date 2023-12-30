ef98d423f29bbc2468ade604b29113cbb72829b7
anupmistry04 <136587110+anupmistry04@users.noreply.github.com>
2023-12-30 12:59:16 +0530

    Update function1.js

--- JavaScript/src/JSBasics/function1.js
+++ JavaScript/src/JSBasics/function1.js
@@ -6,13 +6,14 @@ var fn1=function(v,u){ return v+u};
 
 var fn2=(v,u)=>{ return v+u};
 
-var fn3=(v,u)=>{ return v+u };
+var fn3=(v,u)=>{ return v+u }; OR var fn3=(v,u)=> v+u ;
+
 
 var fn4=v=>{ return v };
 
 var fn5=v=>v;
 
-
+var fn6=v=>console.log(v)
 
 
 
