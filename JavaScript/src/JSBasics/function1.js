4168f2a8f014b98cbeb74f594de1e892ae5e27be
anupmistry04 <136587110+anupmistry04@users.noreply.github.com>
2023-12-30 13:23:43 +0530

    Update function1.js

--- JavaScript/src/JSBasics/function1.js
+++ JavaScript/src/JSBasics/function1.js
@@ -1,19 +1,30 @@
-function fn(a,b){
-    return a+b
+function fn(u,v){
+    return u,v
 }
-
-var fn1=function(v,u){ return v+u};
-
-var fn2=(v,u)=>{ return v+u};
-
-var fn3=(v,u)=>{ return v+u }; OR var fn3=(v,u)=> v+u ;
+var fn=function(v,u){ return v+u};
+var fn=(v,u)=>{ return u+v};
+var fn=(v,u)=> v+u ;
 
 
-var fn4=v=>{ return v };
+var fn1=v=>{ return v };
+var fn1=v=>v;
 
-var fn5=v=>v;
 
-var fn6=v=>console.log(v)
+var fn2=v=>console.log(v)
+
+
+var fn3=number=>number>=0; (return if number greater than equal to 0)
+
+
+aarow fn with no input parameter: 
+var fn4=function(){return Math.random}; 
+var fn4=()=>Math.random;
+
+
+anonymous function:
+var fn5 = function(a,function(){ 
+return a});
+var fn5 = function(a,()=>return a)
 
 
 
